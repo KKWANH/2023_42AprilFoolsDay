3da1ce4964d3d86c41e5c1056064d5071eaee148
KKWANH <kwanho0096@gmail.com>
2023-04-02 15:42:32 +0900

    :sparkles: [ex09] complete/try1

--- ex09/frame.c
+++ ex09/frame.c
@@ -12,31 +12,32 @@ int main(int argc, char *argv[]) {
     if (argc == 1) {
         return 0;
     }
+		for (int str = 0; str < argc; str++) {
+		char **words = malloc((argc-1) * sizeof(char *));
+		int word_count = 0;
+		for (int i = 1; i < argc; i++) {
+			char* token = strtok(argv[i], " ");
+			while (token != NULL) {
+				words[word_count++] = token;
+				token = strtok(NULL, " ");
+			}
+		}
 
-    char **words = malloc((argc-1) * sizeof(char *));
-    int word_count = 0;
-    for (int i = 1; i < argc; i++) {
-        char* token = strtok(argv[i], " ");
-        while (token != NULL) {
-            words[word_count++] = token;
-            token = strtok(NULL, " ");
-        }
-    }
+		int max_length = 0;
+		for (int i = 0; i < word_count; i++) {
+			int length = strlen(words[i]);
+			if (length > max_length) {
+				max_length = length;
+			}
+		}
 
-    int max_length = 0;
-    for (int i = 0; i < word_count; i++) {
-        int length = strlen(words[i]);
-        if (length > max_length) {
-            max_length = length;
-        }
-    }
+		print_line(max_length);
+		for (int i = 0; i < word_count; i++)
+			printf("* %-*s *\n", max_length, words[i]);
+		print_line(max_length);
 
-    print_line(max_length);
-    for (int i = 0; i < word_count; i++)
-        printf("* %-*s *\n", max_length, words[i]);
-    print_line(max_length);
-
-    free(words);
+		free(words);
+	}
 
     return 0;
 }
